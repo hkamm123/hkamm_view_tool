4bf0172a70b1a886355b7f21e3a44892f5822d5e
Nebo Devcamp <nebograngerlomond@devcamp.com>
2017-07-10 17:30:01 +0000

    Dev tool intitial commit

---
+++ lib/hkamm_view_tool.rb
@@ -0,0 +1,6 @@
+require "hkamm_view_tool/version"
+require "hkamm_view_tool/renderer"
+
+module HkammViewTool
+  # Your code goes here...
+end
---
+++ lib/hkamm_view_tool/version.rb
@@ -0,0 +1,3 @@
+module HkammViewTool
+  VERSION = "0.1.0"
+end
